bec05fb0352997d138992c47d31fd859b5c67029
pirnataly <piroznatal@gmail.com>
2023-02-25 20:36:04 +0300

    feat: task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+
+  let absolute=Math.abs(n);
+  let absoluteToArr = String(absolute).split('').reverse().join('');
+  let result = Number(absoluteToArr);
+return result;
+
 }
+
+
+
+
+
